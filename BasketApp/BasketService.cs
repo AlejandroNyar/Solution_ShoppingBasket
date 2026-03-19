5184b85a6a59a15bf5eb5f973bebb3ff14b34f02
AlejandroNyar <lovelesscrafter@gmail.com>
2026-03-19 17:30:04 +0000

    Finished First version of the solution

---
+++ BasketApp/BasketService.cs
@@ -0,0 +1,23 @@
+public class BasketService
+{
+    public List<Item> GetItemsForBasket(List<Item> items, double maxWeight)
+    {
+        //In a real scenario, we retrieve here items from database
+        //For this assignment, the data is provided directly to this method in order to focus on the selection logic.
+        var sortedItems = items.OrderByDescending(i => i.Weight);
+
+        var result = new List<Item>();
+        double currentWeight = 0;
+
+        foreach (var item in sortedItems)
+        {
+            if (currentWeight + item.Weight <= maxWeight)
+            {
+                result.Add(item);
+                currentWeight += item.Weight;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ BasketApp/Item.cs
@@ -0,0 +1,5 @@
+public class Item
+{
+    public required string Name { get; set; }
+    public double Weight { get; set; }
+}
