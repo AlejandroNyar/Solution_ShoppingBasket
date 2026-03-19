e6f959914543063d4b66ae02ae3d4b755c59766f
AlejandroNyar <lovelesscrafter@gmail.com>
2026-03-19 18:34:37 +0000

    Minor changes

--- BasketApp/BasketService.cs
+++ BasketApp/BasketService.cs
@@ -1,10 +1,14 @@
+using System.Linq;
+
 public class BasketService
 {
-    public List<Item> GetItemsForBasket(List<Item> items, double maxWeight)
+    public List<Item> SelectItemsForBasket(List<Item> items, double maxWeight)
     {
         //In a real scenario, we retrieve here items from database
         //For this assignment, the data is provided directly to this method in order to focus on the selection logic.
-        var sortedItems = items.OrderByDescending(i => i.Weight);
+        var sortedItems = items
+            .OrderByDescending(i => i.Weight)
+            .ToList();
 
         var result = new List<Item>();
         double currentWeight = 0;
--- BasketApp/Item.cs
+++ BasketApp/Item.cs
@@ -1,5 +1,7 @@
+using System.Runtime.InteropServices.Marshalling;
+
 public class Item
 {
     public required string Name { get; set; }
-    public double Weight { get; set; }
+    public required double Weight { get; set; }
 }
